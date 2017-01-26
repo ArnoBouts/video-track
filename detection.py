699ddc3627f3077cdcc63f6c11e8e5c0b2c75f01
Arnaud Bouts <arnaud.bouts@softeam.fr>
2017-01-26 10:39:54 +0100

    fix coquille

--- detection.py
+++ detection.py
@@ -69,7 +69,7 @@ class Detection:
             found = False
             for face in self.faces:
                 if(not face.isValid() and newFace.canBe(face)):
-                    self.facesfaces[cptFace] = newFace
+                    self.faces[cptFace] = newFace
                     if(face == self.duckFace):
                         print("Duck Face is", newFace.name, newFace.width, newFace.height)
                         self.duckFace = newFace
